992e578cad2b8d6dc0f353049dfd42336de55b7f
Will Soto <will.soto9@gmail.com>
2014-05-18 08:29:52 -0400

    Add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+cd "$(dirname "${BASH_SOURCE}")"
+git pull origin master
+function bootstrap() {
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+        --exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~
+    source ~/.bash_profile
+}
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+    bootstrap
+else
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        bootstrap
+    fi
+fi
+unset bootstrap

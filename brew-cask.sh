ffd09e39d86eb663b1f9406cdc6b6a63a3dc9b77
Will Soto <will.soto9@gmail.com>
2015-08-16 15:15:44 -0400

    Extract casks and brews to txt files

--- brew-cask.sh
+++ brew-cask.sh
@@ -7,39 +7,10 @@ brew install caskroom/cask/brew-cask
 echo 'Tapping caskroom/versions'
 brew tap caskroom/versions
 
-casks=(
-    # install quick look plugins
-    # https://github.com/sindresorhus/quick-look-plugins
-    'qlcolorcode'
-    'qlstephen'
-    'qlmarkdown'
-    'quicklook-json'
-    'qlprettypatch'
-    'quicklook-csv'
-    'betterzipql'
-    'webp-quicklook'
-    # daily
-    'alfred'
-    'dropbox'
-    # dev
-    'virtualbox'
-    'vagrant'
-    'iterm2'
-    'sublime-text3'
-    # irc
-    # 'limechat'
-    # messaging
-    'hipchat'
-    'slack'
-    # music
-    'soundcleod'
-    'spotify'
-    # browsers
-    'google-chrome-beta'
-    'google-chrome-canary'
-    'firefox'
-    # 'chromium --force'
-);
+echo 'Tapping caskroom/fonts'
+brew tap caskroom/fonts
+
+casks=($(cat casks.txt));
 
 brew cask install ${casks[@]}
 brew cask cleanup

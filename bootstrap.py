3eabbc64cf36a218a6c4f7676d7fecdf4d93ca1f
Will <paradox41@users.noreply.github.com>
2015-08-31 11:07:49 -0400

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -11,10 +11,10 @@ Are you sure? (y/n)
     answer = raw_input(query)
 
     if 'y' in answer.lower():
-        os.symlink('.bash_profile', '~/.bash_profile')
-        os.symlink('.bashrc', '~/.bashrc')
-        os.symlink('.zshrc', '~/.zshrc')
-        os.symlink('themes/', '~/.oh-my-zsh/custom/')
+        # os.symlink('.bash_profile', '~/.bash_profile')
+        # os.symlink('.bashrc', '~/.bashrc')
+        # os.symlink('.zshrc', '~/.zshrc')
+        # os.symlink('themes/', '~/.oh-my-zsh/custom/')
 
         subprocess.call(['bash', 'brew.sh'])
         subprocess.call(['bash', 'brew-cask.sh'])

9c9e86b9cb7f5233ef596ad4bec2423d00420230
Will Soto <will.soto9@gmail.com>
2016-06-14 14:50:04 -0400

    feat(external): add rust bin to PATH
    Signed-off-by: Will Soto <will.soto9@gmail.com>

--- zsh/external.zsh
+++ zsh/external.zsh
@@ -8,4 +8,4 @@ export GREP_OPTIONS='--color=auto --exclude=*.pyc --exclude-dir=.git'
 export GOPATH=$HOME/Projects/go
 
 # path
-export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:$GOPATH/bin"
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:$GOPATH/bin:$HOME/.cargo/bin"

e972dcce126363e0946bd926129282ef3bcc63ee
Will Soto <will.soto9@gmail.com>
2017-01-16 09:35:56 -0500

    feat(external): don't include duplicates in bash history
    Signed-off-by: Will Soto <will.soto9@gmail.com>

--- zsh/external.zsh
+++ zsh/external.zsh
@@ -9,3 +9,6 @@ export GOPATH=$HOME/Projects/go
 
 # path
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:$GOPATH/bin:$HOME/.cargo/bin"
+
+# don't include duplicates in bash history
+export HISTCONTROL=ignoreboth:erasedups
